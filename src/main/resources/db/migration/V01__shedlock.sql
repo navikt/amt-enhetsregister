12629030c49cc1ca9aa9254e26fac0eff14e1e31
Alexander Gård <alex.gaard@hotmail.com>
2022-02-02 13:37:07 +0100

    Rename sql scripts og la til tilgang for cloudsqliamuser

--- src/main/resources/db/migration/V01__shedlock.sql
+++ src/main/resources/db/migration/V01__shedlock.sql
@@ -1,6 +1,6 @@
 CREATE TABLE shedlock
 (
-    name       VARCHAR(64)  NOT NULL,
+    name       VARCHAR(128)  NOT NULL,
     lock_until TIMESTAMP    NOT NULL,
     locked_at  TIMESTAMP    NOT NULL,
     locked_by  VARCHAR(255) NOT NULL,
--- src/main/resources/db/migration/V03__oppdater-enhet-jobb.sql
+++ src/main/resources/db/migration/V03__oppdater-enhet-jobb.sql
@@ -7,11 +7,13 @@ CREATE TABLE delta_enhet_oppdatering
 (
     id                SERIAL PRIMARY KEY,
     oppdatering_id    INT                      NOT NULL,
-    enhet_type        enhet_type               NOT NULL,
+    enhet_type        enhet_type UNIQUE        NOT NULL,
     siste_oppdatering TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Setter oppdatering_id til maks verdi for å forhindre oppdatering helt fra starten.
 -- Når man skal starte delta oppdatering så må man manuelt endre til ønsket id å starte fra
-INSERT INTO delta_enhet_oppdatering (oppdatering_id, enhet_type) VALUES (2147483647, 'MODERENHET');
-INSERT INTO delta_enhet_oppdatering (oppdatering_id, enhet_type) VALUES (2147483647, 'UNDERENHET');
+INSERT INTO delta_enhet_oppdatering (oppdatering_id, enhet_type)
+VALUES (2147483647, 'MODERENHET');
+INSERT INTO delta_enhet_oppdatering (oppdatering_id, enhet_type)
+VALUES (2147483647, 'UNDERENHET');
