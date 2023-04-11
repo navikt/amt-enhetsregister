052a658153be6f92234467adab054c0f022850d8
Tia Firing <tia.firing@nav.no>
2023-04-11 08:53:47 +0200

    oppgraderer dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.2"
+    id("org.springframework.boot") version "3.0.4"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.spring") version "1.7.22"
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.spring") version "1.8.10"
 }
 
 group = "no.nav.amt_enhetsregister"
@@ -18,8 +18,13 @@ repositories {
     maven { setUrl("https://jitpack.io") }
 }
 
-val commonVersion = "2023.02.08_14.02-e5f1a7a4b9e4"
+val commonVersion = "3.2023.04.03_13.09-15bb53f1d80e"
 val testcontainersVersion = "1.17.6"
+val logstashEncoderVersion = "7.3"
+val shedlockVersion = "5.2.0"
+val tokenSupportVersion = "3.0.8"
+val okHttpVersion = "4.10.0"
+val mockOauth2ServerVersion = "0.5.8"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
@@ -31,34 +36,28 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-configuration-processor")
     
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-    implementation("com.github.navikt.common-java-modules:job:$commonVersion") {
-        exclude("com.github.navikt.common-java-modules", "log")
-    }
-    implementation("com.github.navikt.common-java-modules:rest:$commonVersion") {
-        exclude("com.github.navikt.common-java-modules", "log")
-    }
-    implementation("com.github.navikt.common-java-modules:kafka:$commonVersion") {
-        exclude("com.github.navikt.common-java-modules", "log")
-    }
+    implementation("com.github.navikt.common-java-modules:job:$commonVersion")
+    implementation("com.github.navikt.common-java-modules:rest:$commonVersion")
+    implementation("com.github.navikt.common-java-modules:kafka:$commonVersion")
 
     implementation("org.flywaydb:flyway-core")
     implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.31.0")
-    implementation("net.javacrumbs.shedlock:shedlock-core:4.31.0")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.2")
-    implementation("no.nav.security:token-validation-spring:3.0.2")
-    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:$shedlockVersion")
+    implementation("net.javacrumbs.shedlock:shedlock-core:$shedlockVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+    implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
+    implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
     runtimeOnly("org.postgresql:postgresql")
     
     testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
     testImplementation("org.testcontainers:kafka:$testcontainersVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testImplementation("no.nav.security:mock-oauth2-server:0.3.5")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
+    testImplementation("no.nav.security:mock-oauth2-server:$mockOauth2ServerVersion")
+    testImplementation("com.squareup.okhttp3:mockwebserver:$okHttpVersion")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude("com.vaadin.external.google", "android-json")
     }
