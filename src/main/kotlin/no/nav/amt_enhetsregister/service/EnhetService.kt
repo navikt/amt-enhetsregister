ea0b2bb7239d8d3b86aa039d1a28500b09e58669
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2023-08-25 17:15:37 +0200

    Fallback til brreg dersom enhet mangler i db. Oppdater db med manglende enhet.

--- src/main/kotlin/no/nav/amt_enhetsregister/service/EnhetService.kt
+++ src/main/kotlin/no/nav/amt_enhetsregister/service/EnhetService.kt
@@ -1,20 +1,26 @@
 package no.nav.amt_enhetsregister.service
 
+import no.nav.amt_enhetsregister.client.BronnoysundClient
 import no.nav.amt_enhetsregister.kafka.VirksomhetV1Dto
 import no.nav.amt_enhetsregister.repository.EnhetRepository
 import no.nav.amt_enhetsregister.repository.type.UpsertEnhetCmd
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.support.TransactionTemplate
 
 @Service
+
 class EnhetService(
 	private val enhetRepository: EnhetRepository,
+	private val bronnoysundClient: BronnoysundClient,
 	private val kafkaProducerService: KafkaProducerService,
 	private val transactionTemplate: TransactionTemplate,
 	) {
 
+	private val log = LoggerFactory.getLogger(this::class.java)
+
 	fun hentEnhet(organisasjonsnummer: String): EnhetMedOverordnetEnhet? {
-		val enhet = enhetRepository.hentEnhet(organisasjonsnummer) ?: return null
+		val enhet = enhetRepository.hentEnhet(organisasjonsnummer) ?: return fallbackTilBronnoysundOgOppdaterEnhetHvisMangler(organisasjonsnummer)
 
 		val overordnetEnhetNavn = enhet.overordnetEnhet?.let { enhetRepository.hentEnhet(it)?.navn }
 
@@ -24,6 +30,55 @@ class EnhetService(
 			overordnetEnhetOrganisasjonsnummer = enhet.overordnetEnhet,
 			overordnetEnhetNavn = overordnetEnhetNavn
 		)
+	}
+
+	fun fallbackTilBronnoysundOgOppdaterEnhetHvisMangler(organisasjonsnummer: String): EnhetMedOverordnetEnhet? {
+		val underEnhet = bronnoysundClient.hentUnderenhet(organisasjonsnummer)
+		if (underEnhet != null) {
+			log.info("Fant manglende underenhet orgnr=${organisasjonsnummer} i brreg : ${underEnhet} ")
+			oppdaterEnheter(
+				listOf( UpsertEnhet(
+				organisasjonsnummer = organisasjonsnummer,
+				navn = "${underEnhet.navn} ${underEnhet.slettedato != (null ?: "(slettet)")}",
+				overordnetEnhetOrgNr = underEnhet.overordnetEnhet ?: DeltaOppdateringEnhetService.UKJENT_VIRKSOMHET_NR
+			)))
+			if (underEnhet.overordnetEnhet != null) {
+				val moderEnhet = bronnoysundClient.hentModerenhet(underEnhet.overordnetEnhet)
+				if (moderEnhet != null) {
+					return EnhetMedOverordnetEnhet(
+						organisasjonsnummer = organisasjonsnummer,
+						navn = underEnhet.navn,
+						overordnetEnhetOrganisasjonsnummer = underEnhet.overordnetEnhet,
+						overordnetEnhetNavn = moderEnhet.navn
+					)
+				}
+			} else {
+				return EnhetMedOverordnetEnhet(
+					organisasjonsnummer = organisasjonsnummer,
+					navn = underEnhet.navn,
+					overordnetEnhetOrganisasjonsnummer = DeltaOppdateringEnhetService.UKJENT_VIRKSOMHET_NR,
+					overordnetEnhetNavn = null
+				)
+			}
+		}
+		val moderEnhet = bronnoysundClient.hentModerenhet(organisasjonsnummer)
+		if (moderEnhet != null) {
+			log.info("Fant manglende moderenhet orgnr=${organisasjonsnummer} i brreg : ${moderEnhet} ")
+
+			oppdaterEnheter(
+				listOf( UpsertEnhet(
+					organisasjonsnummer = organisasjonsnummer,
+					navn = "${moderEnhet.navn} ${moderEnhet.slettedato != (null ?: "(slettet)")}",
+					overordnetEnhetOrgNr = null
+				)))
+			return EnhetMedOverordnetEnhet(
+				organisasjonsnummer = organisasjonsnummer,
+				navn = moderEnhet.navn,
+				overordnetEnhetOrganisasjonsnummer = null,
+				overordnetEnhetNavn = null
+			)
+		}
+		return null
 	}
 
 	fun oppdaterEnheter(upserts: List<UpsertEnhet>) {
