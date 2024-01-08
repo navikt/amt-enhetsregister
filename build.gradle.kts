2926d1f677de04fc4d1f17c0849a0e8704f8a124
tiafiring <tiafiring@gmail.com>
2024-01-08 10:27:26 +0100

    Java21 (#127)
    * oppgraderer til java 21
    
    * oppdatert tokensupport og mockk
    
    * prøver distroless

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 group = "no.nav.amt_enhetsregister"
 version = "0.0.1-SNAPSHOT"
 description = "ACL mot Brønnøysundregisteret"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
@@ -24,10 +24,10 @@ val commonVersion = "3.2023.12.12_13.53-510909d4aa1a"
 val testcontainersVersion = "1.19.3"
 val logstashEncoderVersion = "7.4"
 val shedlockVersion = "5.10.2"
-val tokenSupportVersion = "3.2.0"
+val tokenSupportVersion = "4.0.3"
 val okHttpVersion = "4.12.0"
 val mockOauth2ServerVersion = "2.1.0"
-val mockkVersion = "1.13.8"
+val mockkVersion = "1.13.9"
 
 extra["logback.version"] = "1.4.14"
 
@@ -81,7 +81,7 @@ tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
