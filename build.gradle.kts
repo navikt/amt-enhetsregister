736ebfbc8797776912fa6f51e56477517054add0
Tormod Fossum <110398791+tofoss@users.noreply.github.com>
2025-06-23 09:24:43 +0200

    Bump dependencies - 20250623091842 (#262)
    * Bump testcontainersVersion from 1.21.1 to 1.21.2
    
    Bumps `testcontainersVersion` from 1.21.1 to 1.21.2.
    
    Updates `org.testcontainers:testcontainers` from 1.21.1 to 1.21.2
    - [Release notes](https://github.com/testcontainers/testcontainers-java/releases)
    - [Changelog](https://github.com/testcontainers/testcontainers-java/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/testcontainers/testcontainers-java/compare/1.21.1...1.21.2)
    
    Updates `org.testcontainers:postgresql` from 1.21.1 to 1.21.2
    - [Release notes](https://github.com/testcontainers/testcontainers-java/releases)
    - [Changelog](https://github.com/testcontainers/testcontainers-java/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/testcontainers/testcontainers-java/compare/1.21.1...1.21.2)
    
    Updates `org.testcontainers:kafka` from 1.21.1 to 1.21.2
    - [Release notes](https://github.com/testcontainers/testcontainers-java/releases)
    - [Changelog](https://github.com/testcontainers/testcontainers-java/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/testcontainers/testcontainers-java/compare/1.21.1...1.21.2)
    
    ---
    updated-dependencies:
    - dependency-name: org.testcontainers:testcontainers
      dependency-version: 1.21.2
      dependency-type: direct:production
      update-type: version-update:semver-patch
    - dependency-name: org.testcontainers:postgresql
      dependency-version: 1.21.2
      dependency-type: direct:production
      update-type: version-update:semver-patch
    - dependency-name: org.testcontainers:kafka
      dependency-version: 1.21.2
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.springframework.boot from 3.5.0 to 3.5.3
    
    Bumps [org.springframework.boot](https://github.com/spring-projects/spring-boot) from 3.5.0 to 3.5.3.
    - [Release notes](https://github.com/spring-projects/spring-boot/releases)
    - [Commits](https://github.com/spring-projects/spring-boot/compare/v3.5.0...v3.5.3)
    
    ---
    updated-dependencies:
    - dependency-name: org.springframework.boot
      dependency-version: 3.5.3
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     val kotlinVersion = "2.1.21"
 
-    id("org.springframework.boot") version "3.5.0"
+    id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
@@ -21,7 +21,7 @@ repositories {
 }
 
 val commonVersion = "3.2024.10.25_13.44-9db48a0dbe67"
-val testcontainersVersion = "1.21.1"
+val testcontainersVersion = "1.21.2"
 val logstashEncoderVersion = "8.1"
 val shedlockVersion = "6.9.0"
 val tokenSupportVersion = "5.0.29"
