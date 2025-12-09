322f38c32ce478e2a88fa742ffe4b52db0d2ce18
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-12-09 18:27:40 +0100

    @ControllerAdvice -> @RestControllerAdvice

--- src/main/kotlin/no/nav/enhetsregister/config/ExceptionHandler.kt
+++ src/main/kotlin/no/nav/enhetsregister/config/ExceptionHandler.kt
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@ControllerAdvice
+@RestControllerAdvice
 class ExceptionHandler {
 
 	private val log = LoggerFactory.getLogger(javaClass)
