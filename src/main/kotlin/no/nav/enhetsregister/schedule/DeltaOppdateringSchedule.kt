2c9b33df624678599287609f13ad6db3b16aac6e
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-12-09 23:21:20 +0100

    Tester uten Kafka

--- src/main/kotlin/no/nav/enhetsregister/schedule/DeltaOppdateringSchedule.kt
+++ src/main/kotlin/no/nav/enhetsregister/schedule/DeltaOppdateringSchedule.kt
@@ -3,9 +3,11 @@ package no.nav.enhetsregister.schedule
 import no.nav.common.job.JobRunner
 import no.nav.common.job.leader_election.LeaderElectionClient
 import no.nav.enhetsregister.service.DeltaOppdateringEnhetService
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
+@ConditionalOnProperty("feature.toggle.enhetsregister", havingValue = "true", matchIfMissing = false)
 @Component
 class DeltaOppdateringSchedule(
 	private val leaderElectionClient: LeaderElectionClient,
