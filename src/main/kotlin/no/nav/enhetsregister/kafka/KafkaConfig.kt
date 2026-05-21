931af6211c191eb3b729ece2f30d18ae687de043
Jarle Fagerheim <jarle.fagerheim@nav.no>
2026-05-21 15:55:49 +0200

    amt-enhetsregister fungerer nesten

--- src/main/kotlin/no/nav/enhetsregister/kafka/KafkaConfig.kt
+++ src/main/kotlin/no/nav/enhetsregister/kafka/KafkaConfig.kt
@@ -1,7 +1,10 @@
 package no.nav.enhetsregister.kafka
 
 import no.nav.common.kafka.producer.KafkaProducerClientImpl
+import no.nav.common.kafka.util.KafkaPropertiesBuilder
 import no.nav.common.kafka.util.KafkaPropertiesPreset
+import org.apache.kafka.common.serialization.ByteArrayDeserializer
+import org.apache.kafka.common.serialization.ByteArraySerializer
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -22,6 +25,24 @@ class KafkaConfig {
 
 		override fun producer(): Properties =
 			KafkaPropertiesPreset.aivenByteProducerProperties("amt-enhetsregister")
+	}
+
+	@Bean
+	@Profile("local")
+	fun kafkaLocalProperties(): KafkaProperties = object : KafkaProperties {
+		override fun consumer(): Properties = KafkaPropertiesBuilder.consumerBuilder()
+			.withBrokerUrl("localhost:9092")
+			.withBaseProperties()
+			.withConsumerGroupId("amt-enhetsregister-local-consumer")
+			.withDeserializers(ByteArrayDeserializer::class.java, ByteArrayDeserializer::class.java)
+			.build()
+
+		override fun producer(): Properties = KafkaPropertiesBuilder.producerBuilder()
+			.withBrokerUrl("localhost:9092")
+			.withBaseProperties()
+			.withProducerId("amt-enhetsregister-local-producer")
+			.withSerializers(ByteArraySerializer::class.java, ByteArraySerializer::class.java)
+			.build()
 	}
 
 	@Bean
